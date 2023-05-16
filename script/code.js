c5fbeb2acd8f54ebcbfb02a6a6ad0ff0ebd4550b
SethJKallis <kallisseth@gmail.com>
2023-05-16 13:00:03 +0200

    commit to put branch ahead

--- script/code.js
+++ script/code.js
@@ -1,4 +1,5 @@
 const subjectArray = ['Python', 'C++', 'VB.Net', 'JavaScript', 'Assembly'];
+
 console.log(subjectArray.splice(0,1));
 console.log(subjectArray.splice(subjectArray.length-1,1));
 console.log(subjectArray.splice((subjectArray.length-1)/2,1));
