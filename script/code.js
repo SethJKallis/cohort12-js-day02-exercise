da80694fb01260526590da6974582dba518ecc5c
SethJKallis <kallisseth@gmail.com>
2023-05-16 14:00:47 +0200

    removed unneeded code

--- script/code.js
+++ script/code.js
@@ -1,11 +1,9 @@
 const subjectArray = ['Python', 'C++', 'VB.Net', 'JavaScript', 'Assembly'];
 
-const firstValue = subjectArray.splice(0,1, 'Python');
 const middleValue = subjectArray.splice(-1,1, 'Assembly');
 const lastValue = subjectArray.splice((subjectArray.length-1)/2,1, 'VB.Net');
 
-console.log(firstValue);
 console.log(middleValue);
 console.log(lastValue);
 
-console.log(subjectArray)
+console.log(subjectArray);
